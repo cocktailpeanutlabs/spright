e7cd9874dc67a31f1d64035ff4bfd12c6a0570bf
cocktailpeanut <cocktailpeanuts@proton.me>
2024-04-02 13:13:05 -0400

    init

---
+++ pinokio.js
@@ -0,0 +1,64 @@
+const path = require('path')
+module.exports = {
+  version: "1.5",
+  title: "spright",
+  description: "a framework to improve the spatial consistency of text-to-image models WITHOUT compromising their fidelity aspects https://huggingface.co/spaces/SPRIGHT-T2I/SPRIGHT-T2I",
+  icon: "icon.webp",
+  menu: async (kernel) => {
+    let installing = await kernel.running(__dirname, "install.js")
+    let installed = await kernel.exists(__dirname, "app", "env")
+    let running = await kernel.running(__dirname, "start.js")
+    if (installing) {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running) {
+        let local = kernel.memory.local[path.resolve(__dirname, "start.js")]
+        if (local && local.url) {
+          return [{
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else {
+        return [{
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+        }]
+      }
+    } else {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
+    }
+  }
+}
---
+++ start.js
@@ -0,0 +1,31 @@
+module.exports = async (kernel) => {
+  let script = {
+    daemon: true,
+    run: [{
+      method: "shell.run",
+      params: {
+        path: "app",
+        venv: "env",
+        env: {
+          CACHE_EXAMPLES: "0"
+        },
+        message: [
+          "python app.py",
+        ],
+        on: [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+      }
+    }, {
+      "method": "local.set",
+      "params": {
+        "url": "{{input.event[0]}}"
+      }
+    }, {
+      "method": "proxy.start",
+      "params": {
+        "uri": "{{local.url}}",
+        "name": "Local Sharing"
+      }
+    }]
+  }
+  return script
+}
